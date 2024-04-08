261ecb460f3a1fe01d775c008d944f73453fdb3e
Gabe005 <103907860+Gabe005@users.noreply.github.com>
2024-04-08 17:52:09 +0800

    Update index.py

--- index.py
+++ index.py
@@ -69,7 +69,4 @@ def upload_file():
 def download_file(filename):
     return send_file(filename, as_attachment=True)
 
-if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+
