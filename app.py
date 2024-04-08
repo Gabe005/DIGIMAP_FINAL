b1659ab41c44404b1acb4c46b42f4a871c3dde4b
Gabriele De Guzman <gabr.degu@gmail.com>
2024-04-08 18:52:41 +0800

    added logging

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from flask_cors import CORS
 import os
 import cv2 as cv
 from cv2 import dnn_superres
+import logging
+
+# Set up logging configuration
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 CORS(app)
 
 def enhance_image(image_path):
+    logger.debug('Enhancing image: %s', image_path)
+
     # initialize super resolution object
     sr = dnn_superres.DnnSuperResImpl_create()
 
@@ -27,6 +34,8 @@ def enhance_image(image_path):
     """image = cv.imread(image_path)
     bicubic = cv.resize(image, (upscaled.shape[1], upscaled.shape[0]), interpolation=cv.INTER_CUBIC)"""
 
+    logger.debug('Image enhancement complete')
+
     return upscaled
 
 @app.route('/')
@@ -35,23 +44,29 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    logger.debug('Received upload request')
     if 'imageFile' not in request.files:
+        logger.error('No file part in the request')
         return jsonify({'error': 'No file part'})
 
     file = request.files['imageFile']
     if file.filename == '':
+        logger.error('No selected file')
         return jsonify({'error': 'No selected file'})
 
     if file:
         img_path = os.path.join(app.instance_path, file.filename)
+        logger.debug('Saving uploaded file to %s', img_path)
         file.save(img_path)
 
         enhanced_img = enhance_image(img_path)
 
         # Save enhanced image to a temporary file
         enhanced_img_path = os.path.join(app.instance_path, 'enhanced_' + file.filename)
+        logger.debug('Saving enhanced image to %s', enhanced_img_path)
         cv.imwrite(enhanced_img_path, enhanced_img)
 
+        logger.debug('Upload processing complete')
         return jsonify({'enhanced_image_path': enhanced_img_path})
 
 @app.route('/download/<path:filename>', methods=['GET'])
